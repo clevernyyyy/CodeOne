07f3415cc2071289cf264a3c4ddc1d6b0b71fec1
Treger <graysmg3@gmail.com>
2014-09-12 20:12:20 -0700

    Minor Connection Fix

--- CodeOne/Classes/Connection.vb
+++ CodeOne/Classes/Connection.vb
@@ -1,7 +1,6 @@
 ﻿Imports System.Data.SqlClient
 Public Class Connection
-    Dim cnnStr As String = "server = 184.168.47.13; database = codelogically_survey; UID = codeOne; pwd = AC3sFa32;"
-    Dim cnnLogin As String = "server = 184.168.47.21; database = codinglogical_login; UID = loginlogically; pwd = tj7B3J7kAC3sFa32;"
+    Dim cnnStr As String = "server = 184.168.47.21; database = codinglogical_login; UID = loginlogically; pwd = tj7B3J7kAC3sFa32;"
     Public ReadOnly Property Str As String
         Get
             Return cnnStr
@@ -10,11 +9,6 @@ Public Class Connection
     Public ReadOnly Property NewCnn As SqlConnection
         Get
             Return New SqlConnection(cnnStr)
-        End Get
-    End Property
-    Public ReadOnly Property NewCnnLogin As SqlConnection
-        Get
-            Return New SqlConnection(cnnLogin)
         End Get
     End Property
     Public Sub New(cnnStr As String)
