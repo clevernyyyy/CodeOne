2f6d2ece83d77f403aecd49e6af64ac41498f40c
Adam Schaal <aschaal1263@gmail.com>
2014-09-14 05:55:14 -0500

    Updates vbproj

---
+++ CodeOne/Scripts/WebForms/Menu/Budgets.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('.select-green input').iCheck({
+        checkboxClass: 'icheckbox_square-green',
+        radioClass: 'iradio_square-green',
+        increaseArea: '20%'
+    });
+    $('.select-blue input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%'
+    });
+});
